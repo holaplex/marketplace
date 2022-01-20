2e7ed3c19c0ccb558542bad703a237e58e9e980c
sjc <email@sjc.sh>
2022-01-20 01:20:40 -0600

    make title a link to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { WalletConnect } from '../../components/WalletConnect'
 import { DarkModeToggle } from '../DarkModeToggle'
+import Link from 'next/link'
 
 type Props = {
   title: string
@@ -8,7 +9,11 @@ type Props = {
 export const Header = ({ title }: Props) => {
   return (
     <div className="flex items-center justify-between py-6 px-8 overlay-1">
-      <h1 className="text-xl font-semibold">{title}</h1>
+      <Link href="/">
+        <a>
+          <h1 className="text-xl font-semibold">{title}</h1>
+        </a>
+      </Link>
       <div className="flex items-center justify-end gap-6">
         <DarkModeToggle />
         <WalletConnect />
