3e220be44145bfbbf8085fb1b12eb988d7ec2f9e
Damian Rebman <damian@Damians-MBP-4.attlocal.net>
2022-02-17 17:28:56 -0800

    Changes colors in tw theme. Minor style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,42 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'black': '#000000',
+      'white': '#ffffff',
+      'gray': {
+          100: '#E0E0E0',
+          300: '#A8A8A8',
+          500: '#6F6F6F',
+          800: '#262626',
+          900: '#171717',
+      },
+      'error': {
+          300: '#FDA29B',
+          500: '#F04438',
+          800: '#912018',
+          900: '#7A271A'
+      },
+      'success': {
+          300: '#6CE9A6',
+          500: '#12B76A',
+          800: '#05603A',
+          900: '#054F31'
+      }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   variants: {},
   plugins: [],
