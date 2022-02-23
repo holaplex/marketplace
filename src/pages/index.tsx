b2eb66bcce2a16df50fca2911240c2a467fd3acd
Damian Rebman <damian@Damians-MBP-4.attlocal.net>
2022-02-22 23:27:45 -0800

    Max width layout and some styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -167,7 +167,7 @@ const Home: NextPage<HomePageProps> = ({ storefront }) => {
   const [showXsFilter, setShowXsFilter] = useState(false)
 
   return (
-    <div className='text-white bg-gray-900'>
+    <div className='text-white bg-gray-900 flex flex-col items-center'>
       {showXsFilter && (
         <div className='fixed z-20 w-full h-full px-4 py-4 pt-24 bg-black'>
           <button
@@ -199,15 +199,15 @@ const Home: NextPage<HomePageProps> = ({ storefront }) => {
             <div className='flex flex-col flex-grow mb-6'>
               <div className='flex justify-between w-full p-2 rounded-md'>
                 <h4>Current listings</h4>
-                <span>0</span>
+                <span className='text-sm text-gray-500'>0</span>
               </div>
               <div className='flex justify-between w-full p-2 rounded-md'>
                 <h4>Owned by me</h4>
-                <span>0</span>
+                <span className='text-sm text-gray-500'>0</span>
               </div>
               <div className='flex justify-between w-full p-2 bg-gray-800 rounded-md'>
                 <h4>Unlisted</h4>
-                <span>0</span>
+                <span className='text-sm text-gray-500'>0</span>
               </div>
             </div>
             <div className='flex flex-col flex-grow gap-4'>
@@ -257,59 +257,60 @@ const Home: NextPage<HomePageProps> = ({ storefront }) => {
           </form>
         </div>
       )}
-      <div
-        className='relative flex items-start justify-end p-6 bg-center bg-cover h-60'
-        style={{ backgroundImage: `url(${storefront.bannerUrl})` }}
-      >
-        <div className='flex items-center justify-end gap-6'>
+      
+      <div className='w-full relative'>
+        <div className="absolute right-8 top-8">
           <WalletMultiButton>Connect</WalletMultiButton>
         </div>
-        <Link href='/'>
-          <a
-            className='absolute h-24 bg-black bg-center bg-cover rounded-full -bottom-12 left-6 aspect-square border-4 border-black'
-            style={{ backgroundImage: `url(${storefront.logoUrl})` }}
-          ></a>
-        </Link>
+
+        <img src={storefront.bannerUrl} alt={storefront.title} className='w-full h-80 object-cover'/>
       </div>
-      <div className='flex justify-between px-6 mt-20 mb-10'>
-        <div className='flex-col'>
+
+      <div className='max-w-[1800px] px-8'>
+        
+        <div className='flex flex-col justify-between mt-20 mb-20 w-full relative'>
+          <img 
+            src={storefront.logoUrl} 
+            alt={storefront.title}
+            className='w-28 h-28 rounded-full border-4 border-gray-900 absolute -top-32'
+          />
           <h1>{storefront.title}</h1>
-          <p className='mt-4'>{storefront.description}</p>
+          <p className='mt-4 max-w-prose'>{storefront.description}</p>
         </div>
-      </div>
-      <div className='w-full pr-4 md:pr-8 lg:pr-8  xs:mb-[75px]'>
+
         <div className='flex'>
           <button
-            className='fixed rounded-full text-black bg-white h-10 right-[25%] w-[50%] z-10 block sm:hidden md:hidden lg:hidden xl:hidden 2xl:hidden bottom-5'
+            className='fixed rounded-full text-black bg-white h-10 right-[25%] w-[50%] z-10 block sm:hidden bottom-5'
             onClick={() => setShowXsFilter(true)}
           >
             Filter
           </button>
-          <div className='flex-row flex-none hidden px-6 space-y-2 w-72 sm:block md:block lg:block xl:block 2xl:block'>
+          
+          <div className='flex-row flex-none hidden space-y-2 w-64 mr-10 sm:block'>
             <form
               onSubmit={e => {
                 e.preventDefault()
               }}
             >
               <div className='flex flex-col flex-grow mb-6'>
-                <div className='flex justify-between w-full p-2 rounded-md'>
+                <div className='flex justify-between w-full py-2 px-4 mb-1 rounded-md cursor-pointer hover:bg-gray-800'>
                   <h4>Current listings</h4>
-                  <span>0</span>
+                  <span className='text-sm text-gray-500'>0</span>
                 </div>
-                <div className='flex justify-between w-full p-2 rounded-md'>
+                <div className='flex justify-between w-full py-2 px-4 mb-1 rounded-md cursor-pointer hover:bg-gray-800'>
                   <h4>Owned by me</h4>
-                  <span>0</span>
+                  <span className='text-sm text-gray-500'>0</span>
                 </div>
-                <div className='flex justify-between w-full p-2 bg-gray-800 rounded-md'>
+                <div className='flex justify-between w-full py-2 px-4 mb-1 bg-gray-800 rounded-md cursor-pointer hover:bg-gray-800'>
                   <h4>Unlisted</h4>
-                  <span>0</span>
+                  <span className='text-sm text-gray-500'>0</span>
                 </div>
               </div>
               <div className='flex flex-col flex-grow gap-4'>
                 {sidebar.data?.creator.attributeGroups.map(
                   ({ name: group, variants }, index) => (
                     <div className='flex flex-col flex-grow gap-2' key={group}>
-                      <label>
+                      <label className='label'>
                         {group.charAt(0).toUpperCase() + group.slice(1)}
                       </label>
 
@@ -354,56 +355,52 @@ const Home: NextPage<HomePageProps> = ({ storefront }) => {
               </div>
             </form>
           </div>
-          <div className='ml-4 grow'>
+          <div className='grow'>
             {nfts.loading ? (
               <>Loading</>
             ) : (
               <div className='container md:mx-auto lg:mx-auto'>
-                <div className='flex flex-wrap -mx-1 lg:-mx-4'>
+                <div className='grid grid-cols-1 gap-8 2xl:gap-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4'>
                   {nfts.data?.nfts.map(n => (
-                    <div className='w-full px-1 my-1 md:w-1/2 md:pb-2 md:px-2 lg:mb-4 lg:px-2 lg:w-1/3 xl:w-1/4 2xl:w-1/5'>
-                      <Link href={'/nfts/' + n.address}>
-                        <article className='overflow-hidden rounded-lg transition duration-300 transform hover:scale-[1.02]'>
-                          <div className='h-[300px] overflow-hidden'>
-                            <img
-                              alt='Placeholder'
-                              className='block w-full h-full'
-                              src={n.image as string}
-                            />
-                          </div>
-                          <header className='p-4 border border-t-0 border-b-0 border-[#262626]'>
-                            <p className='lg:text-base mb-2 text-sm truncate ...'>
-                              {n.name}
-                            </p>
-                            <p className='text-sm text-[#a8a8a8]'>
-                              <span>
-                                {' '}
-                                <img
-                                  src={storefront.logoUrl}
-                                  className='object-fill rounded-sm inline-block h-[20px] mr-2'
-                                />
-                              </span>
-                              {storefront.title}
-                            </p>
-                          </header>
+                    <Link href={'/nfts/' + n.address}>
+                      <article className='overflow-hidden rounded-lg transition duration-100 transform cursor-pointer bg-gray-900 shadow-card	hover:scale-[1.02]'>
+                        <img
+                          alt='Placeholder'
+                          className='block w-full aspect-square'
+                          src={n.image as string}
+                        />
+                        <header className='p-4'>
+                          <p className='lg:text-base mb-2 text-sm truncate ...'>
+                            {n.name}
+                          </p>
+                          <p className='text-sm text-[#a8a8a8]'>
+                            <span>
+                              {' '}
+                              <img
+                                src={storefront.logoUrl}
+                                className='object-fill rounded-full inline-block h-[16px] mr-1'
+                              />
+                            </span>
+                            {storefront.title}
+                          </p>
+                        </header>
 
-                          <footer className='grid grid-cols-2 gap-2 p-4 bg-[#262626] rounded-t-none rounded-b-lg'>
-                            <div>
-                              <p className='text-[#a8a8a8] text-sm'>
-                                Current Bid
-                              </p>
-                              <p className='text-sm'>{solSymbol} 33</p>
-                            </div>
-                            <div>
-                              <p className='text-right text-[#a8a8a8] text-sm'>
-                                Ends In
-                              </p>
-                              <p className='text-sm text-right '>19h 48m 53s</p>
-                            </div>
-                          </footer>
-                        </article>
-                      </Link>
-                    </div>
+                        <footer className='grid grid-cols-2 gap-2 p-4 bg-[#262626] rounded-t-none rounded-b-lg'>
+                          <div>
+                            <p className='text-[#a8a8a8] text-sm'>
+                              Current Bid
+                            </p>
+                            <p className='text-sm'>{solSymbol} 33</p>
+                          </div>
+                          <div>
+                            <p className='text-right text-[#a8a8a8] text-sm'>
+                              Ends In
+                            </p>
+                            <p className='text-sm text-right '>19h 48m 53s</p>
+                          </div>
+                        </footer>
+                      </article>
+                    </Link>
                   ))}
                 </div>
               </div>
