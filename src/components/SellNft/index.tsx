f705c799d2c6f0200872867eef90927c06c983c8
Jason Schorr <echohtp@gmail.com>
2022-02-28 16:12:27 -0400

    clean sell action

--- src/components/SellNft/index.tsx
+++ src/components/SellNft/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState }  from 'react'
 import { useForm, Controller } from 'react-hook-form';
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { Link } from 'react-router-dom';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { NATIVE_MINT } from '@solana/spl-token'
+import { AuctionHouseProgram } from '@metaplex-foundation/mpl-auction-house'
+import { Transaction, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js'
+import BN from 'bn.js'
+
+const { createSellInstruction } = AuctionHouseProgram.instructions
 
 interface OfferForm {
   amount: string;
@@ -9,20 +15,123 @@ interface OfferForm {
 
 interface OfferProps {
   nft: any;
+  ah: any;
 }
 
-const SellNft = ({ nft }: OfferProps) => {
+const SellNft = ({ nft, ah }: OfferProps) => {
   const { control, watch } = useForm<OfferForm>({});
+  const { publicKey, signTransaction } = useWallet()
+  const { connection } = useConnection()
+  const [sellAmount, setSellAmount] = useState(0)
+
+  const sellNftTransaction = async () => {
+    const sellPrice = String(Number(sellAmount) * LAMPORTS_PER_SOL)
+    const tokenSize = '1'
+    const auctionHouse = new PublicKey(ah.address)
+    const authority = new PublicKey(ah.authority)
+    const auctionHouseFeeAccount = new PublicKey(ah.auction_house_fee_account)
+
+    const tokenMint = new PublicKey(nft.mintAddress)
+
+
+    if (!publicKey || !signTransaction) {
+      return
+    }
+
+    const associatedTokenAccount = (
+      await AuctionHouseProgram.getAtaForMint(tokenMint, new PublicKey(nft.owner.address)) 
+    )[0] 
+
+
+    // Find TradeState Account
+    const [
+      sellerTradeState,
+      tradeStateBump,
+    ] = await AuctionHouseProgram.findTradeStateAccount(
+      publicKey,
+      auctionHouse,
+      associatedTokenAccount, // VERIFY THIS
+      NATIVE_MINT,
+      tokenMint,
+      sellPrice,
+      tokenSize
+    )
+
+
+    const metadata = await AuctionHouseProgram.getMetadata(tokenMint)
+
+    const [
+      programAsSigner,
+      programAsSignerBump,
+    ] = await AuctionHouseProgram.getAuctionHouseProgramAsSigner()
+
+    const [
+      freeTradeState,
+      freeTradeBump,
+    ]  = await AuctionHouseProgram.getAuctionHouseTradeState(
+      auctionHouse,
+      publicKey,
+      associatedTokenAccount,
+      NATIVE_MINT,
+      tokenMint,
+      1,
+      0
+    )
+
+    // make transaction
+    const txt = new Transaction()
+
+    const sellInstructionArgs = {
+      tradeStateBump,
+      freeTradeStateBump: Number(freeTradeBump),
+      programAsSignerBump: programAsSignerBump,
+      buyerPrice: new BN(sellPrice),
+      tokenSize: new BN(tokenSize),
+    }
+
+    const sellInstructionAccounts = {
+      wallet: publicKey,
+      tokenAccount: associatedTokenAccount,
+      metadata: metadata,
+      authority: authority,
+      auctionHouse: auctionHouse,
+      auctionHouseFeeAccount: auctionHouseFeeAccount,
+      sellerTradeState: sellerTradeState,
+      freeSellerTradeState: freeTradeState,
+      programAsSigner: programAsSigner,
+    }
+
+    // generate instruction
+    const instruction = createSellInstruction(
+      sellInstructionAccounts,
+      sellInstructionArgs
+    )
+
+    // add instruction to tx
+    txt.add(instruction)
+
+    // lookup recent block hash and assign fee payer (the current logged in user)
+    txt.recentBlockhash = (await connection.getRecentBlockhash()).blockhash
+    txt.feePayer = publicKey
+
+    // sign it
+    const signed = await signTransaction(txt)
+
+    // submit transaction
+    const signature = await connection.sendRawTransaction(signed.serialize())
+    await connection.confirmTransaction(signature, 'processed')
+  }
 
   return (
     <form
       className="text-left"
       onSubmit={(e) => {
         e.preventDefault();
+        sellNftTransaction();
       }
       }>
-      <h3 className="text-xl md:text-2xl font-bold mb-6">Sell this Nft</h3>
-      <label className="mb-1 block">Price in SOL</label>
+      <h3 className="mb-6 text-xl font-bold md:text-2xl">Sell this Nft</h3>
+      <label className="block mb-1">Price in SOL</label>
       <div className="prefix-input prefix-icon-sol">
         <Controller
           control={control}
@@ -43,7 +152,7 @@ const SellNft = ({ nft }: OfferProps) => {
                   onChange={(e: any) => {
                     onChange(e.target.value);
                   }}
-                  className="bg-transparent focus:outline-none mb-4 pl-8 border-gray-500 border-2 w-full h-10 rounded-md"
+                  className="w-full h-10 pl-8 mb-4 bg-transparent border-2 border-gray-500 rounded-md focus:outline-none"
                 />
                 <div className="flex flex-col gap-2 mb-4">
                   <div className="flex justify-between">
@@ -73,11 +182,11 @@ const SellNft = ({ nft }: OfferProps) => {
           }}
         />
       </div>
-      <div className="grid grid-cols-2 flex-grow gap-4">
+      <div className="grid flex-grow grid-cols-2 gap-4">
         <Link to={`/nfts/${nft.address}`}>
-          <button className="text-sm text-white w-full transition-colors duration-150 bg-black rounded-full h-12 lg:text-xl md:text-base focus:shadow-outline hover:bg-black">Cancel</button>
+          <button className="w-full h-12 text-sm text-white transition-colors duration-150 bg-black rounded-full lg:text-xl md:text-base focus:shadow-outline hover:bg-black">Cancel</button>
         </Link>
-        <button className="text-sm text-black transition-colors duration-150 bg-white rounded-full h-12 lg:text-xl md:text-base focus:shadow-outline hover:bg-white">List for sale</button>
+        <button className="h-12 text-sm text-black transition-colors duration-150 bg-white rounded-full lg:text-xl md:text-base focus:shadow-outline hover:bg-white">List for sale</button>
       </div>
     </form>
   )
