b36da050cc78b1ca891cda6209a524ec91c0babf
Jason Schorr <echohtp@gmail.com>
2022-03-01 17:22:52 -0400

    didnt need the import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import Select from 'react-select'
 import { useForm, Controller } from 'react-hook-form'
 import client from '../client'
 import { useState } from 'react'
-import { Marketplace, Creator, Nft, AuctionHouse } from '../types';
+import { Marketplace, Creator, Nft } from '../types';
 
 const SUBDOMAIN = process.env.MARKETPLACE_SUBDOMAIN
 
