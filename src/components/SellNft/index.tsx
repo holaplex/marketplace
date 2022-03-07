58c8c1a5a7f2653ea9144d492d1875fc42a48a0f
Jason Schorr <echohtp@gmail.com>
2022-03-07 16:36:15 -0400

    Basic toast popups

--- src/components/SellNft/index.tsx
+++ src/components/SellNft/index.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import { useForm, Controller } from 'react-hook-form'
-import { Link } from 'react-router-dom'
-import { useConnection, useWallet } from '@solana/wallet-adapter-react'
-import { AuctionHouseProgram } from '@metaplex-foundation/mpl-auction-house'
-import { MetadataProgram } from '@metaplex-foundation/mpl-token-metadata'
-import { Transaction, PublicKey, LAMPORTS_PER_SOL, SYSVAR_INSTRUCTIONS_PUBKEY } from '@solana/web3.js'
-import { Nft, Marketplace } from '../../types'
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { AuctionHouseProgram } from '@metaplex-foundation/mpl-auction-house';
+import { MetadataProgram } from '@metaplex-foundation/mpl-token-metadata';
+import { Transaction, PublicKey, LAMPORTS_PER_SOL, SYSVAR_INSTRUCTIONS_PUBKEY } from '@solana/web3.js';
+import { Nft, Marketplace } from '../../types';
+import { toast } from 'react-toastify';
 
 const { createSellInstruction, createPrintListingReceiptInstruction } = AuctionHouseProgram.instructions
 
@@ -116,9 +117,8 @@ const SellNft = ({ nft, marketplace }: SellNftProps) => {
     txt.feePayer = publicKey
 
     const signed = await signTransaction(txt)
-
     const signature = await connection.sendRawTransaction(signed.serialize());
-
+    toast("Transaction Sent!");
     await connection.confirmTransaction(signature, 'processed');
   }
 
