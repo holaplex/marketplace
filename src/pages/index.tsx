492b94388ec88f06469ed8bed419ffe2c6ebe72d
Cosimo <cosimo.solana@gmail.com>
2022-03-26 11:01:17 -0400

    Reduce header image height a bit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -268,7 +268,7 @@ const Home: NextPage<HomePageProps> = ({ marketplace }) => {
         <img
           src={marketplace.bannerUrl}
           alt={marketplace.name}
-          className="object-cover w-full h-44 md:h-60 lg:h-80 xl:h-[20rem] 2xl:h-[28rem]"
+          className="object-cover w-full h-30 md:h-44 lg:h-60 xl:h-[18rem]"
         />
       </div>
       <div className="w-full max-w-[1800px] px-8">
