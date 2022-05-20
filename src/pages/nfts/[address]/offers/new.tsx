22cac0919008f82bde1548f974ddc4f9ba11733a
Anshul Goel <anshul.go87@gmail.com>
2022-05-20 13:10:07 +0530

    refactor(offers): use offers action from marketplace sdk

--- src/pages/nfts/[address]/offers/new.tsx
+++ src/pages/nfts/[address]/offers/new.tsx
@@ -5,21 +5,15 @@ import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import { AuctionHouseProgram } from '@metaplex-foundation/mpl-auction-house'
-import { MetadataProgram } from '@metaplex-foundation/mpl-token-metadata'
 import { gql, useQuery } from '@apollo/client'
-import {
-  Transaction,
-  PublicKey,
-  LAMPORTS_PER_SOL,
-  SYSVAR_INSTRUCTIONS_PUBKEY,
-} from '@solana/web3.js'
 import { toast } from 'react-toastify'
 import { NftLayout } from './../../../../layouts/Nft'
 import client from './../../../../client'
-import { Nft, Marketplace, GetNftData } from './../../../../types'
+import { GetNftData, Marketplace, Nft } from './../../../../types'
 import Button, { ButtonType } from './../../../../components/Button'
 import { useLogin } from '../../../../hooks/login'
+import { Wallet } from '@metaplex/js'
+import { OffersClient, Nft as NftFromSdk } from '@holaplex/marketplace-js-sdk'
 
 const SUBDOMAIN = process.env.MARKETPLACE_SUBDOMAIN
 
@@ -187,14 +181,8 @@ export async function getServerSideProps({ req, query }: NextPageContext) {
   }
 }
 
-const {
-  createPublicBuyInstruction,
-  createPrintBidReceiptInstruction,
-  createDepositInstruction,
-} = AuctionHouseProgram.instructions
-
 interface OfferForm {
-  amount: string
+  amount: number
 }
 
 interface OfferProps {
@@ -208,7 +196,8 @@ const OfferNew = ({ nft, marketplace }: OfferProps) => {
     register,
     formState: { isSubmitting },
   } = useForm<OfferForm>({})
-  const { publicKey, signTransaction } = useWallet()
+  const wallet = useWallet()
+  const { publicKey, signTransaction } = wallet
   const { connection } = useConnection()
   const router = useRouter()
   const login = useLogin()
@@ -223,118 +212,28 @@ const OfferNew = ({ nft, marketplace }: OfferProps) => {
       return
     }
 
-    const buyerPrice = Number(amount) * LAMPORTS_PER_SOL
-    const auctionHouse = new PublicKey(marketplace.auctionHouse.address)
-    const authority = new PublicKey(marketplace.auctionHouse.authority)
-    const auctionHouseFeeAccount = new PublicKey(
-      marketplace.auctionHouse.auctionHouseFeeAccount
-    )
-    const treasuryMint = new PublicKey(marketplace.auctionHouse.treasuryMint)
-    const tokenMint = new PublicKey(nft.mintAddress)
-    const tokenAccount = new PublicKey(nft.owner.associatedTokenAccountAddress)
-
-    const [escrowPaymentAccount, escrowPaymentBump] =
-      await AuctionHouseProgram.findEscrowPaymentAccountAddress(
-        auctionHouse,
-        publicKey
-      )
-
-    const [buyerTradeState, tradeStateBump] =
-      await AuctionHouseProgram.findPublicBidTradeStateAddress(
-        publicKey,
-        auctionHouse,
-        treasuryMint,
-        tokenMint,
-        buyerPrice,
-        1
-      )
-
-    const [metadata] = await MetadataProgram.findMetadataAccount(tokenMint)
-
-    const txt = new Transaction()
-
-    const depositInstructionAccounts = {
-      wallet: publicKey,
-      paymentAccount: publicKey,
-      transferAuthority: publicKey,
-      treasuryMint,
-      escrowPaymentAccount,
-      authority,
-      auctionHouse,
-      auctionHouseFeeAccount,
-    }
-    const depositInstructionArgs = {
-      escrowPaymentBump,
-      amount: buyerPrice,
-    }
-
-    const depositInstruction = createDepositInstruction(
-      depositInstructionAccounts,
-      depositInstructionArgs
-    )
-
-    const publicBuyInstruction = createPublicBuyInstruction(
-      {
-        wallet: publicKey,
-        paymentAccount: publicKey,
-        transferAuthority: publicKey,
-        treasuryMint,
-        tokenAccount,
-        metadata,
-        escrowPaymentAccount,
-        authority,
-        auctionHouse,
-        auctionHouseFeeAccount,
-        buyerTradeState,
-      },
-      {
-        escrowPaymentBump,
-        tradeStateBump,
-        tokenSize: 1,
-        buyerPrice,
-      }
-    )
-
-    const [receipt, receiptBump] =
-      await AuctionHouseProgram.findBidReceiptAddress(buyerTradeState)
-
-    const printBidReceiptInstruction = createPrintBidReceiptInstruction(
-      {
-        receipt,
-        bookkeeper: publicKey,
-        instruction: SYSVAR_INSTRUCTIONS_PUBKEY,
-      },
-      {
-        receiptBump,
-      }
-    )
-
-    txt
-      .add(depositInstruction)
-      .add(publicBuyInstruction)
-      .add(printBidReceiptInstruction)
-
-    txt.recentBlockhash = (await connection.getRecentBlockhash()).blockhash
-    txt.feePayer = publicKey
-
-    let signed: Transaction | undefined = undefined
+    // try {
+    //   signed = await signTransaction(txt)
+    // } catch (e: any) {
+    //   toast.error(e.message)
+    //   return
+    // }
+    // let signature: string | undefined = undefined
 
     try {
-      signed = await signTransaction(txt)
-    } catch (e: any) {
-      toast.error(e.message)
-      return
-    }
+      /** TODO: To show the following toast, we might need to get 
+       callback from sdk once the transaction is signed 
+       */
+      // toast('Sending the transaction to Solana.')
+      // signature = await connection.sendRawTransaction(signed.serialize())
+      // await connection.confirmTransaction(signature, 'confirmed')
 
-    let signature: string | undefined = undefined
-
-    try {
-      toast('Sending the transaction to Solana.')
-
-      signature = await connection.sendRawTransaction(signed.serialize())
-
-      await connection.confirmTransaction(signature, 'confirmed')
-
+      const offersClient = new OffersClient(
+        connection,
+        wallet as Wallet,
+        marketplace.auctionHouse
+      )
+      await offersClient.make({ amount, nft: nft as NftFromSdk })
       toast.success('The transaction was confirmed.')
     } catch (e: any) {
       toast.error(e.message)
@@ -365,6 +264,7 @@ const OfferNew = ({ nft, marketplace }: OfferProps) => {
           {...register('amount', { required: true })}
           autoFocus
           className="input"
+          type="number"
           placeholder="Price in SOL"
         />
       </div>
